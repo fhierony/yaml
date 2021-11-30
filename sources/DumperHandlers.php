2189d73b7dec482d3cf40e8c40b4a542106d8b65
fhierony <fhierony@disroot.org>
2021-11-30 11:15:14 +0100

    refactor: Fix a PHP Notice
    Fix issue #9 (https://github.com/dallgoot/yaml/issues/9)

--- sources/DumperHandlers.php
+++ sources/DumperHandlers.php
@@ -1,7 +1,6 @@
 <?php
-namespace Dallgoot\Yaml;
 
-use Dallgoot\Yaml\Dumper;
+namespace Dallgoot\Yaml;
 
 /**
  *  Convert PHP datatypes to a YAML string syntax
@@ -20,27 +19,29 @@ class DumperHandlers
     }
 
 
-    public function dumpScalar($dataType):string
+    public function dumpScalar($dataType): string
     {
         if ($dataType === \INF) return '.inf';
         if ($dataType === -\INF) return '-.inf';
-        $precision = "%.".$this->dumper->floatPrecision."F";
+        $precision = "%." . $this->dumper->floatPrecision . "F";
         switch (gettype($dataType)) {
-            case 'boolean': return $dataType ? 'true' : 'false';
+            case 'boolean':
+                return $dataType ? 'true' : 'false';
             case 'float': //fall through
-            case 'double': return is_nan((double) $dataType) ? '.nan' : sprintf($precision, $dataType);
+            case 'double':
+                return is_nan((double)$dataType) ? '.nan' : sprintf($precision, $dataType);
         }
         return $this->dumpString($dataType);
     }
 
 
-    public function dumpCompound($compound, int $indent):string
+    public function dumpCompound($compound, int $indent): string
     {
         if ($this->dumper->_compactMode) {
             return $this->dumpCompact($compound, $indent);
         } else {
             if (is_array($compound)) {
-                if ($compound[0] instanceof YamlObject) {
+                if (isset($compound[0]) && $compound[0] instanceof YamlObject) {
                     return $this->dumper->dumpMultiDoc($compound);
                 }
                 $iterator = new \ArrayIterator($compound);
@@ -57,7 +58,7 @@ class DumperHandlers
         throw new \Exception("Dumping Callable|Resource is not currently supported", 1);
     }
 
-    private function dumpObject($object, int $indent):string
+    private function dumpObject($object, int $indent): string
     {
         if ($object instanceof YamlObject) {
             return $this->dumper->dumpYamlObject($object);
@@ -76,19 +77,18 @@ class DumperHandlers
     }
 
 
-
     /**
      * Dumps a Compact|mixed (representing an array or object) as the single-line format representation.
      * All values inside are assumed single-line as well.
      * Note: can NOT use JSON_encode because of possible reference calls or definitions as : '&abc 123', '*fre'
      * which would be quoted by json_encode
      *
-     * @param mixed   $subject The subject
-     * @param integer $indent  The indent
+     * @param mixed $subject The subject
+     * @param integer $indent The indent
      *
      * @return string the string representation (JSON like) of the value
      */
-    public function dumpCompact($subject, int $indent):string
+    public function dumpCompact($subject, int $indent): string
     {
         $structureFormat = '{%s}';
         $keyMask = "%s: ";
@@ -104,7 +104,7 @@ class DumperHandlers
             }
         }
         $previousCompactMode = $this->dumper->_compactMode;
-        $this->dumper->_compactMode =  true;
+        $this->dumper->_compactMode = true;
         $result = $this->dumper->iteratorToString(new \ArrayIterator($source), $keyMask, ', ', $indent);
         $this->dumper->_compactMode = $previousCompactMode;
         return sprintf($structureFormat, $result);
@@ -113,12 +113,12 @@ class DumperHandlers
     /**
      * Dumps a string. Protects it if needed
      *
-     * @param      string  $str    The string
+     * @param string $str The string
      *
      * @return     string  ( description_of_the_return_value )
      * @todo   implements checking and protection function
      */
-    public function dumpString(string $str):string
+    public function dumpString(string $str): string
     {
         //those characters must be escaped : - : ? { } [ ] # , & * ! > | ' " %
         // The “@” (#x40, at) and “`” (#x60, grave accent) are reserved for future use.
@@ -126,17 +126,17 @@ class DumperHandlers
         // Example 5.13. Escaped Characters
 
         $str = json_encode(ltrim($str));
-        return strspn(substr($str,1,-1), "-:?{}[]#,&*!>|'\"%") > 0 ? $str : trim($str, '"');
+        return strspn(substr($str, 1, -1), "-:?{}[]#,&*!>|'\"%") > 0 ? $str : trim($str, '"');
     }
 
-    public function dumpTagged(Tagged $obj, int $indent):string
+    public function dumpTagged(Tagged $obj, int $indent): string
     {
-        $separator   = ' ';
+        $separator = ' ';
         $valueIndent = 0;
         if (!is_scalar($obj->value) && !$this->dumper->_compactMode) {
             $separator = "\n";
             $valueIndent = $indent + $this->dumper::INDENT;
         }
-        return $obj->tagName.$separator.$this->dumper->dump($obj->value, $valueIndent);
+        return $obj->tagName . $separator . $this->dumper->dump($obj->value, $valueIndent);
     }
 }
